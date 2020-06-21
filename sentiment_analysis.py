f0b3214e33c5779890f9e714871456c9ebf8009a
Christos Ponirakos <cponirakos@gmail.com>
2020-06-21 09:56:00 +0300

    update naming conventions

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -21,16 +21,16 @@ def extract_features(corpus):
     return processed_corpus
 
 
-data_directory = 'movie_reviews'
-movie_sentiment_data = datasets.load_files(data_directory, shuffle=True)
-print('{} files loaded.'.format(len(movie_sentiment_data.data)))
+data_directory = 'tweets'
+tweets_sent_data = datasets.load_files(data_directory, shuffle=True)
+print('{} files loaded.'.format(len(tweets_sent_data.data)))
 print('They contain the following classes: {}.'.format(
-    movie_sentiment_data.target_names))
+    tweets_sent_data.target_names))
 
-movie_tfidf = extract_features(movie_sentiment_data.data)
+tweets_tfidf = extract_features(tweets_sent_data.data)
 
 X_train, X_test, y_train, y_test = model_selection.train_test_split(
-    movie_tfidf, movie_sentiment_data.target, test_size=0.30, random_state=42)
+    tweets_tfidf, tweets_sent_data.target, test_size=0.30, random_state=42)
 
 
 model = linear_model.LogisticRegression()
